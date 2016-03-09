3b767c231c5bc6e3fd55dc8d4af0ca6fbb97fbf8
meandors <meandors@tlen.pl>
2016-03-09 17:08:13 +0100

    master

---
+++ JP_lab2_03_2016/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace JP_lab2_03_2016
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.bPrzegladaj = new System.Windows.Forms.Button();
+            this.bOdczyt = new System.Windows.Forms.Button();
+            this.bZapis = new System.Windows.Forms.Button();
+            this.rbPlik = new System.Windows.Forms.RadioButton();
+            this.rbPamiec = new System.Windows.Forms.RadioButton();
+            this.cbSzyfrowanie = new System.Windows.Forms.CheckBox();
+            this.cbKompresja = new System.Windows.Forms.CheckBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(118, 12);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(327, 20);
+            this.tbPath.TabIndex = 0;
+            this.tbPath.Text = "C:\\";
+            // 
+            // bPrzegladaj
+            // 
+            this.bPrzegladaj.Location = new System.Drawing.Point(451, 12);
+            this.bPrzegladaj.Name = "bPrzegladaj";
+            this.bPrzegladaj.Size = new System.Drawing.Size(75, 23);
+            this.bPrzegladaj.TabIndex = 1;
+            this.bPrzegladaj.Text = "Przegladaj";
+            this.bPrzegladaj.UseVisualStyleBackColor = true;
+            this.bPrzegladaj.Click += new System.EventHandler(this.bPrzegladaj_Click);
+            // 
+            // bOdczyt
+            // 
+            this.bOdczyt.Location = new System.Drawing.Point(451, 41);
+            this.bOdczyt.Name = "bOdczyt";
+            this.bOdczyt.Size = new System.Drawing.Size(75, 23);
+            this.bOdczyt.TabIndex = 2;
+            this.bOdczyt.Text = "Odczyt";
+            this.bOdczyt.UseVisualStyleBackColor = true;
+            this.bOdczyt.Click += new System.EventHandler(this.bOdczyt_Click);
+            // 
+            // bZapis
+            // 
+            this.bZapis.Location = new System.Drawing.Point(451, 70);
+            this.bZapis.Name = "bZapis";
+            this.bZapis.Size = new System.Drawing.Size(75, 23);
+            this.bZapis.TabIndex = 3;
+            this.bZapis.Text = "Zapis";
+            this.bZapis.UseVisualStyleBackColor = true;
+            this.bZapis.Click += new System.EventHandler(this.bZapis_Click);
+            // 
+            // rbPlik
+            // 
+            this.rbPlik.AutoSize = true;
+            this.rbPlik.Checked = true;
+            this.rbPlik.Location = new System.Drawing.Point(27, 15);
+            this.rbPlik.Name = "rbPlik";
+            this.rbPlik.Size = new System.Drawing.Size(42, 17);
+            this.rbPlik.TabIndex = 4;
+            this.rbPlik.TabStop = true;
+            this.rbPlik.Text = "Plik";
+            this.rbPlik.UseVisualStyleBackColor = true;
+            // 
+            // rbPamiec
+            // 
+            this.rbPamiec.AutoSize = true;
+            this.rbPamiec.Location = new System.Drawing.Point(27, 38);
+            this.rbPamiec.Name = "rbPamiec";
+            this.rbPamiec.Size = new System.Drawing.Size(60, 17);
+            this.rbPamiec.TabIndex = 5;
+            this.rbPamiec.Text = "Pamięć";
+            this.rbPamiec.UseVisualStyleBackColor = true;
+            // 
+            // cbSzyfrowanie
+            // 
+            this.cbSzyfrowanie.AutoSize = true;
+            this.cbSzyfrowanie.Location = new System.Drawing.Point(36, 86);
+            this.cbSzyfrowanie.Name = "cbSzyfrowanie";
+            this.cbSzyfrowanie.Size = new System.Drawing.Size(83, 17);
+            this.cbSzyfrowanie.TabIndex = 6;
+            this.cbSzyfrowanie.Text = "Szyfrowanie";
+            this.cbSzyfrowanie.UseVisualStyleBackColor = true;
+            // 
+            // cbKompresja
+            // 
+            this.cbKompresja.AutoSize = true;
+            this.cbKompresja.Location = new System.Drawing.Point(122, 86);
+            this.cbKompresja.Name = "cbKompresja";
+            this.cbKompresja.Size = new System.Drawing.Size(75, 17);
+            this.cbKompresja.TabIndex = 7;
+            this.cbKompresja.Text = "Kompresja";
+            this.cbKompresja.UseVisualStyleBackColor = true;
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(36, 109);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(409, 93);
+            this.tbResult.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 229);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.cbKompresja);
+            this.Controls.Add(this.cbSzyfrowanie);
+            this.Controls.Add(this.rbPamiec);
+            this.Controls.Add(this.rbPlik);
+            this.Controls.Add(this.bZapis);
+            this.Controls.Add(this.bOdczyt);
+            this.Controls.Add(this.bPrzegladaj);
+            this.Controls.Add(this.tbPath);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Strumienie";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button bPrzegladaj;
+        private System.Windows.Forms.Button bOdczyt;
+        private System.Windows.Forms.Button bZapis;
+        private System.Windows.Forms.RadioButton rbPlik;
+        private System.Windows.Forms.RadioButton rbPamiec;
+        private System.Windows.Forms.CheckBox cbSzyfrowanie;
+        private System.Windows.Forms.CheckBox cbKompresja;
+        private System.Windows.Forms.TextBox tbResult;
+    }
+}
+
---
+++ JP_lab2_03_2016/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using System.Security.Cryptography;
+
+namespace JP_lab2_03_2016
+{
+    public partial class Form1 : Form
+    {
+        MemoryStream memory;
+        public Form1()
+        {
+            InitializeComponent();
+            memory = new MemoryStream();
+        }
+
+        private void bPrzegladaj_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            fileDialog.FilterIndex = 1;
+            fileDialog.RestoreDirectory = true;
+            fileDialog.ShowDialog();
+
+            tbPath.Text = fileDialog.FileName;
+
+        }
+
+        private void bOdczyt_Click(object sender, EventArgs e)
+        {
+            if (rbPlik.Checked)
+            {
+                if (File.Exists(tbPath.Text))
+                {
+                    using (FileStream fileStream = new FileStream(tbPath.Text, FileMode.Open))
+                    {
+
+                        if (cbKompresja.Checked && cbSzyfrowanie.Checked)
+                        { 
+                            decryptAndDecompress(fileStream);
+                        }
+                        else if (cbKompresja.Checked && !cbSzyfrowanie.Checked)
+                        {
+                            decompress(fileStream);
+                        }
+                        else if (!cbKompresja.Checked && cbSzyfrowanie.Checked)
+                        {
+                            decrypt(fileStream);
+                        }
+                        else
+                        {
+                            using (StreamReader reader = new StreamReader(fileStream))
+                            {
+                                tbResult.Text = reader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+                else if (rbPamiec.Checked)
+                {
+                    using (MemoryStream memoryStream = new MemoryStream())
+                    {
+                        if (cbKompresja.Checked && cbSzyfrowanie.Checked)
+                        { 
+                            decryptAndDecompress(memoryStream);
+                        }
+                        else if (cbKompresja.Checked && !cbSzyfrowanie.Checked)
+                        {
+                            decompress(memoryStream);
+                        }
+                        else if (!cbKompresja.Checked && cbSzyfrowanie.Checked)
+                        {
+                            decrypt(memoryStream);
+                        }
+                        else
+                        {
+                            using (StreamReader reader = new StreamReader(memoryStream))
+                            {
+                                tbResult.Text = reader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Brak pliku o podanej nazwie");
+                }
+            }
+        }
+
+        private void bZapis_Click(object sender, EventArgs e)
+        {
+            if (rbPlik.Checked)
+            {
+                using (FileStream fileStream = new FileStream(tbPath.Text, FileMode.Create))
+                {
+                    if (cbKompresja.Checked && cbSzyfrowanie.Checked)
+                    {
+                        encryptAndCompress(tbResult.Text, fileStream);
+                    }
+                    else if (cbKompresja.Checked && !cbSzyfrowanie.Checked)
+                    {
+                        compress(tbResult.Text, fileStream);
+                    }
+                    else if (!cbKompresja.Checked && cbSzyfrowanie.Checked)
+                    {
+                        encrypt(tbResult.Text, fileStream);
+                        tbResult.Text = "";
+
+                    }
+                    else
+                    {
+                        using (StreamWriter writer = new StreamWriter(fileStream))
+                        {
+                            writer.Write(tbResult.Text);
+                            tbResult.Text = "";
+                            MessageBox.Show("Zapisano do pliku!");
+                        }
+                    }
+                }
+            }
+            else if (rbPamiec.Checked)
+            {
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    if (cbKompresja.Checked && cbSzyfrowanie.Checked)
+                    {
+                        encryptAndCompress(tbResult.Text, memoryStream);
+                    }
+                    else if (cbKompresja.Checked && !cbSzyfrowanie.Checked)
+                    {
+                        compress(tbResult.Text, memoryStream);
+                    }
+                    else if (!cbKompresja.Checked && cbSzyfrowanie.Checked)
+                    {
+                        encrypt(tbResult.Text, memoryStream);
+                        tbResult.Text = "";
+                    }
+                    else
+                    {
+                        using (StreamWriter writer = new StreamWriter(memoryStream))
+                        {
+                            writer.Write(tbResult.Text);
+                            tbResult.Text = "";
+                            MessageBox.Show("Zapisano do pliku!");
+                        }
+                    }
+                }
+            }
+        }
+        public void decompress(Stream stream)
+        {
+            using (DeflateStream deflateStream = new DeflateStream(stream, CompressionMode.Decompress))
+            {
+                using (StreamReader reader = new StreamReader(deflateStream))
+                {
+                    tbResult.Text = reader.ReadToEnd();
+                }
+            }
+        }
+        public void compress(string text, Stream stream)
+        {
+            using (DeflateStream deflateStream = new DeflateStream(stream, CompressionMode.Compress))
+            {
+                using (StreamWriter write = new StreamWriter(deflateStream))
+                {
+                    write.Write(tbResult.Text);
+                    tbResult.Text = "";
+                }
+            }
+        }
+        public void encrypt(string text, Stream stream)
+        {
+            DESCryptoServiceProvider cryptic = new DESCryptoServiceProvider();
+
+            cryptic.Key = ASCIIEncoding.ASCII.GetBytes("ABCDEFGH");
+            cryptic.IV = ASCIIEncoding.ASCII.GetBytes("ABCDEFGH");
+
+            using (CryptoStream cryptoStream = new CryptoStream(stream, cryptic.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                using(StreamWriter writer = new StreamWriter(cryptoStream)){
+                    writer.Write(text);
+                    tbResult.Text = "";
+                }
+            }
+        }
+        public void decrypt(Stream stream)
+        {
+            
+            DESCryptoServiceProvider cryptic = new DESCryptoServiceProvider();
+
+            cryptic.Key = ASCIIEncoding.ASCII.GetBytes("ABCDEFGH");
+            cryptic.IV = ASCIIEncoding.ASCII.GetBytes("ABCDEFGH");
+
+            using (CryptoStream cryptoStream = new CryptoStream(stream, cryptic.CreateDecryptor(), CryptoStreamMode.Read))
+            {
+                using (StreamReader reader = new StreamReader(cryptoStream))
+                {
+                    tbResult.Text = reader.ReadToEnd();
+                }
+            }
+        }
+        private void encryptAndCompress(string text, Stream stream)
+        {
+            DESCryptoServiceProvider cryptic = new DESCryptoServiceProvider();
+
+            cryptic.Key = ASCIIEncoding.ASCII.GetBytes("ABCDEFGH");
+            cryptic.IV = ASCIIEncoding.ASCII.GetBytes("ABCDEFGH");
+
+            using (CryptoStream cryptoStream = new CryptoStream(stream, cryptic.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                using (DeflateStream compress = new DeflateStream(cryptoStream, CompressionMode.Compress))
+                using (StreamWriter writer = new StreamWriter(cryptoStream))
+                {
+                    writer.Write(text);
+                    tbResult.Text = "";
+                }
+            }
+        }
+        private void decryptAndDecompress(Stream stream)
+        {
+            DESCryptoServiceProvider cryptic = new DESCryptoServiceProvider();
+
+            cryptic.Key = ASCIIEncoding.ASCII.GetBytes("ABCDEFGH");
+            cryptic.IV = ASCIIEncoding.ASCII.GetBytes("ABCDEFGH");
+            
+            using (CryptoStream cryptoStream = new CryptoStream(stream, cryptic.CreateDecryptor(), CryptoStreamMode.Read))
+            {
+                using (DeflateStream decompress = new DeflateStream(cryptoStream, CompressionMode.Decompress))
+                    using (StreamReader reader = new StreamReader(cryptoStream))
+                    {
+                        tbResult.Text = reader.ReadToEnd();
+                    }
+            }
+        }
+    }
+}
